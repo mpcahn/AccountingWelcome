e8c7feeff2b6962252a5374cf306927fc5a50f67
mpcahn <mpcahn@gmail.com>
2018-10-11 14:58:15 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountingWelcome
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems");
+            Console.WriteLine("Remember, We're \"Accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
